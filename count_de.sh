601a62b0819f544e30bd87bca571ad29c8525392
evadyadko <58735048+evadyadko@users.noreply.github.com>
2021-03-09 23:01:34 +0100

    Update count_de.sh

--- count_de.sh
+++ count_de.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-DIR=$1
-
-
-function h1 {
-	echo ${FUNCNAME[0]}
-	cat $DIR | grep -Eo '\w+' | grep -i -w "de" | wc -w
-}
+# This shell script counts the occurences of the article 'de' in a text file.
+file=$1
 
 
-h1
+count_de () (
+	echo 'Amount that de has occured: '
+	cat $file | grep -Eo '\w+' | tr '[:upper:]' '[:lower:]' | grep -iw "de" | wc -w
+)
+
+
+count_de
